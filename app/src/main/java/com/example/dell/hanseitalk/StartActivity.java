36a80f4bd4f9c1d04e41f2fc213d765dec9f6815
GGeonu <chrismin7030@naver.com>
2019-04-07 00:51:28 +0900

    Start and Chat

---
+++ app/src/main/java/com/example/dell/hanseitalk/StartActivity.java
@@ -0,0 +1,86 @@
+package com.example.dell.hanseitalk;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class StartActivity extends AppCompatActivity {
+
+
+    private EditText user_chat, user_edit;
+    private Button user_next;
+    private ListView chat_list;
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        user_chat = findViewById(R.id.user_chat);
+        user_edit = findViewById(R.id.user_edit);
+        user_next = findViewById(R.id.user_next);
+        chat_list = findViewById(R.id.chat_list);
+
+        user_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (user_edit.getText().toString().equals("") || user_chat.getText().toString().equals("")){
+                    return;
+                }
+                Intent intent = new Intent(StartActivity.this, ChatActivity.class);
+                intent.putExtra("chatName", user_chat.getText().toString());
+                intent.putExtra("userName", user_edit.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+        showChatList();
+    }
+
+
+    private void showChatList(){
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        chat_list.setAdapter(adapter);
+
+        databaseReference.child("chat").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                adapter.add(dataSnapshot.getKey());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
